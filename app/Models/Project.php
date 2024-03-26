56e6b4402e1906dfbba24db296c94d91806f1eed
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-03-26 01:45:47 +0100

    feat; seeder and migration

---
+++ app/Models/Project.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title', 
+        'thumb',
+        'descriptions',
+        'languages',
+        'slug',
+    ];
+
+    public static function generateSlug($title){
+        return Str::slug($title, '-');
+    }
+}
